54a331e320dc601e5cb4225411848b75ef2cf905
Thawitchai Boonsong <std.64122420210@ubru.ac.th>
2025-07-18 20:05:47 +0700

    API IN OUT Employee
    - คนมา
    - คนลา
    - คนขาด
    
    ** ส่วนคนไม่จำเป็น ต้อง Getsunova **

---
+++ src/app/_components/ChartMonth.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChartMonth = () => {
+       return (
+              <div>
+                     <h1>ChartMonth</h1>
+              </div>
+       );
+}
+
+export default ChartMonth;
